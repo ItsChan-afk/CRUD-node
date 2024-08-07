6263ab0667d2351aa94107ce70955f082fa3a576
Mac <chanminswe26@gmail.com>
2024-08-07 23:13:50 +0630

    added custom middle ware and asyn handler

--- controllers/userInformationController.js
+++ controllers/userInformationController.js
@@ -1,24 +1,35 @@
-const getInformations = (req , res) => {
+const asyncHandler = require('express-async-handler')
+
+const getInformations = asyncHandler( async (req , res) => {
     res.status(200).json({message : 'Get User Informations'})
-}
+})
 
-const createInformation = (req , res) => {
+const createInformation = asyncHandler (async (req , res) => {
+    console.log(req.body)
+
+    //validating information
+    const {name , email , password} = req.body;
+    if(!name || !email || !password){
+        res.status(400);
+        throw new Error('All the fields are necessary !')
+    }
+
     res.status(201).json({message : 'Get User Informations'})
-}
+})
 
-const getInformation = (req , res) => {
+const getInformation =  asyncHandler(async (req , res) => {
     const id = req.params
     res.status(200).json({message : `Get Information about User Id : ${id}`})
-}
+})
 
-const updateInformation = (req , res) => {
+const updateInformation = asyncHandler(async (req , res) => {
     const id = req.params
     res.status(200).json({message : `Update Information about User Id : ${id}`})
-}
+})
 
-const deleteInformation =  (req , res) => {
+const deleteInformation =  asyncHandler(async (req , res) => {
     const id = req.params
     res.status(200).json({message : `Delete Information about User Id : ${id}`})
-}
+})
 
 module.exports = {getInformations , createInformation , getInformation , updateInformation , deleteInformation}
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express')
+const errorHandler = require('./middleware/errorHandler')
 const app = express()
 const dotenv = require('dotenv').config()
 
 const PORT = process.env.PORT || 4000
 
+app.use(express.json())
 
 app.use('/api/userinformations' , require('./routes/informationRoutes'))
+
+app.use((req , res , next) => {
+    res.status(404)
+    throw new Error('Not Found');
+})
+
+app.use(errorHandler)
 
 app.listen(PORT , () => {
     console.log(`PORT : ${PORT} has started`)
