d4821504c59fc449eb8f6c0c0cb3b4f0e40479a5
Mac <chanminswe26@gmail.com>
2024-08-08 22:27:00 +0630

    Before inserting jwt to our code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler');
 const Users = require('../models/UserModel');
+const bcrypt = require('bcrypt')
 
 const loginUser = asyncHandler( async (req , res) => {
     res.status(201).json({message : `Login User Successfully`})
 })
 
 const registerUser = asyncHandler(async (req , res) => {
+
     //checking empty fields
     const {username , email , password } = req.body;
     if(!username || !email || !password){
@@ -17,20 +19,32 @@ const registerUser = asyncHandler(async (req , res) => {
     const userAvailable = await Users.findOne({
         email
     })
-
     if(userAvailable){
         res.status(400)
         throw new Error("User already registered")
     }
 
-    const register = await Users.create({
+    //crypting the password
+    const hashPassword = await bcrypt.hash(password ,10)
+    console.log("Password is " , hashPassword)
+
+    const user = await Users.create({
         username,
         email,
-        password
-    })
+        hashPassword
+    });
 
+    console.log('User Created')
 
-    res.status(201).json(register)
+    //to show back the data to the user
+    if(user){
+        res.status(201).json({_id: user.id , email : user.email})
+    }
+    else{
+        res.status(400);
+        throw new Error("User Data is not valid")
+    }
+    res.status(201).json(user)
 })
 
 const currentUser = asyncHandler(async (req , res) => {
